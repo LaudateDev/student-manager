77d4c9180665c13a6b8da2885aa98f91afdbd256
Beni Tasisu <emmanuel.tasisu@gmail.com>
2026-02-26 08:36:34 +0100

    installation de Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    './resources/views/**/*.edge', // toutes tes vues Edge
+    './resources/js/**/*.js', // tes scripts JS
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
